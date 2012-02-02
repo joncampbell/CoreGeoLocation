91ad5e1ddf0a044f2fd3d1977a32df2b6fa3fe9d
Jon Campbell <joncampbell@gmail.com>
2012-02-02 08:54:50 -0800

    Updated leader file

--- Source/CoreGeoLocation.h
+++ Source/CoreGeoLocation.h
@@ -6,9 +6,9 @@
 //  Copyright 2009 Karl Adam. BSD Licensed, check ReadMe.markdown for details
 //
 
-#import <CoreGeoLocation/CGLGeoManager.h>
-#import <CoreGeoLocation/CGLGeoRequest.h>
-#import <CoreGeoLocation/CGLGeoLocation.h>
-#import <CoreGeoLocation/CGLGeoDataProvider.h>
-#import <CoreGeoLocation/CGLGeoDataProviderYahoo.h>
-#import <CoreGeoLocation/CGLGeoDataProviderGoogle.h>
+#import "CGLGeoManager.h"
+#import "CGLGeoRequest.h"
+#import "CGLGeoLocation.h"
+#import "CGLGeoDataProvider.h"
+#import "CGLGeoDataProviderYahoo.h"
+#import "CGLGeoDataProviderGoogle.h"
